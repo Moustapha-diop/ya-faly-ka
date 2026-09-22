9c33b7dd0c69a590e5b4b3c6375e425e2f03ff4a
moustapha-diop <moistaphadiop57@gmail.com>
2026-09-22 21:07:57 +0000

    Configure exact Google Maps GPS coordinates pin for YA FALY KA store

--- js/products.js
+++ js/products.js
@@ -17,7 +17,7 @@ const STORE_CONFIG = {
   tiktokUrl: "https://www.tiktok.com/@globaldecoameublemant?_r=1&_t=ZS-99tOu0NYh4C",
   facebookUrl: "https://www.facebook.com/share/1MhaQLFsY6/",
   instagramUrl: "https://www.instagram.com/kayafaly?utm_source=qr&stkn=MWZiZDIxZnFheXhncQ==",
-  googleMapsUrl: "https://www.google.com/maps/search/?api=1&query=Grand+Mbao+Cite+Baye+Niasse+Dakar"
+  googleMapsUrl: "https://maps.google.com/?q=14.7359609,-17.3143774&entry=gps"
 };
 
 const IMG = "assets/images/products/";
